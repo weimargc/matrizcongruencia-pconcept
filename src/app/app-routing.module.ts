c73e164f983177dad54ea5d0c7fd1f643ee2dc5b
Weimar <weimar.gomez@loganitsolutions.com>
2022-03-26 14:35:58 -0500

    Configuraciones Project / boostrap/Material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatrizComponent } from './components/matriz/matriz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'matrix',component: MatrizComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatrizComponent } from './components/matriz/matriz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatrizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
